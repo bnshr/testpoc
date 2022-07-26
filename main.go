74ad6531cbe154796cc98e0db7162b51be1cb5f2
bnshr <bmandal@redhat.com>
2022-07-26 14:45:17 +0200

    reflection

--- main.go
+++ main.go
@@ -1,42 +1,68 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"time"
+	"reflect"
 )
 
-func enrichContext(ctx context.Context) context.Context {
-	return context.WithValue(ctx, "request-id", "12345")
-}
+type stubMapping map[string]interface{}
 
-func doSomethingCool(ctx context.Context) {
-	requestId := ctx.Value("request-id")
-	fmt.Println(requestId)
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("timed out inside doSomethingCool")
-			return
-		default:
-			fmt.Println("doing something cool")
-		}
-		time.Sleep(500 * time.Millisecond)
+var StrubStorage = stubMapping{}
+
+func setup() {
+
+	StrubStorage = map[string]interface{}{
+		"longRunningTest1": longRunningTest1,
+		"longRunningTest2": longRunningTest2,
 	}
 }
-
 func main() {
-	fmt.Println("Go context tutorial")
+	setup()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	ctx = enrichContext(ctx)
-	go doSomethingCool(ctx)
+	/*
+		Check for the enable of the tests.
+		For each enable status, call the function - Open/Close design principle (not checked)
 
-	select {
-	case <-ctx.Done():
-		fmt.Println("oh no ..")
+		Keep a counter for enable tests
+
+		If error is found, return and cancel using Context
+
+	*/
+
+	result1, _ := Call("longRunningTest1", "max", "mustermann")
+
+	outputStr1 := result1.(string)
+	fmt.Println(outputStr1)
+
+	result2, _ := Call("longRunningTest2", 100)
+
+	outputStr2 := result2.(int)
+	fmt.Println(outputStr2)
+}
+
+func Call(funcName string, params ...interface{}) (result interface{}, err error) {
+	f := reflect.ValueOf(StrubStorage[funcName])
+
+	if len(params) != f.Type().NumIn() {
+		err = errors.New("The number of params is out of index")
+		return
 	}
 
-	time.Sleep(5 * time.Second)
+	in := make([]reflect.Value, len(params))
+	for k, param := range params {
+		in[k] = reflect.ValueOf(param)
+	}
+
+	res := f.Call(in)
+	result = res[0].Interface()
+	return
+}
+
+func longRunningTest1(a, b string) string {
+	return fmt.Sprintf("Done for %v, %v", a, b)
+}
+
+func longRunningTest2(val int) int {
+	return 10 * val
 }
---
+++ main_cool.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func enrichContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, "request-id", "12345")
+}
+
+func doSomethingCool(ctx context.Context) {
+	requestId := ctx.Value("request-id")
+	fmt.Println(requestId)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("timed out inside doSomethingCool")
+			fmt.Println(ctx.Err())
+			return
+		default:
+			fmt.Println("doing something cool")
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func main_cool() {
+	fmt.Println("Go context tutorial")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	ctx = enrichContext(ctx)
+	go doSomethingCool(ctx)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("oh no ..")
+	}
+
+	time.Sleep(5 * time.Second)
+}
